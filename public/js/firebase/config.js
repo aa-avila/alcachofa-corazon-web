5d369bfee39fd09de7620823b5ba570346a5a716
Agustin Avila <agustin.avila.dev@gmail.com>
2022-09-22 19:21:39 -0300

    config firebase credentials

--- public/js/firebase/config.js
+++ public/js/firebase/config.js
@@ -4,17 +4,9 @@ import {
   getAnalytics,
   logEvent
 } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-analytics.js';
+import { firebaseCredentials } from './credentials.js';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAYwSSLcb71LiTxVP__g1k7sQZJ5TAdNnk',
-  authDomain: 'alcachofa-corazon.firebaseapp.com',
-  databaseURL: 'https://alcachofa-corazon-default-rtdb.firebaseio.com',
-  projectId: 'alcachofa-corazon',
-  storageBucket: 'alcachofa-corazon.appspot.com',
-  messagingSenderId: '1034140561735',
-  appId: '1:1034140561735:web:5f443a0bc566dd94fb5c62',
-  measurementId: 'G-L02R05D4W4'
-};
+const firebaseConfig = { ...firebaseCredentials };
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
