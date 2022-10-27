8dfce755af872237503e8936e258065755b2d02c
Agustín Avila Humerez <agustin.avila.dev@gmail.com>
2022-10-27 15:46:02 -0300

    Create credentials.js

---
+++ public/js/firebase/credentials.js
@@ -0,0 +1,10 @@
+export const firebaseCredentials = {
+  apiKey: 'AIzaSyAYwSSLcb71LiTxVP__g1k7sQZJ5TAdNnk',
+  authDomain: 'alcachofa-corazon.firebaseapp.com',
+  databaseURL: 'https://alcachofa-corazon-default-rtdb.firebaseio.com',
+  projectId: 'alcachofa-corazon',
+  storageBucket: 'alcachofa-corazon.appspot.com',
+  messagingSenderId: '1034140561735',
+  appId: '1:1034140561735:web:5f443a0bc566dd94fb5c62',
+  measurementId: 'G-L02R05D4W4'
+}
