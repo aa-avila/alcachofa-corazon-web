2d3ca9b092a12d21a12b447f6dbd14f23b4423cb
Agustin Avila <agustin.avila.dev@gmail.com>
2023-08-23 13:01:23 -0300

    fix: change images host

--- public/js/utils/getImgHost.js
+++ public/js/utils/getImgHost.js
@@ -1,10 +1,12 @@
 import { HOSTS } from './constants.js';
-import { fetchGet } from './fetchData.js';
+// import { fetchGet } from './fetchData.js';
 
-export const getImgHost = async () => {
-  const { data: primaryStatus } = await fetchGet(HOSTS.IMAGES_PRIMARY);
-  if (primaryStatus === 'OK') {
-    return HOSTS.IMAGES_PRIMARY;
-  }
-  return HOSTS.IMAGES_SECONDARY;
-};
+export const getImgHost = async () => HOSTS.IMAGES_PRIMARY;
+
+// export const getImgHost = async () => {
+//   const { data: primaryStatus } = await fetchGet(HOSTS.IMAGES_PRIMARY);
+//   if (primaryStatus === 'OK') {
+//     return HOSTS.IMAGES_PRIMARY;
+//   }
+//   return HOSTS.IMAGES_SECONDARY;
+// };
